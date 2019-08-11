89e002464db3d294bd395a5cfa8033473bfd1bb6
Anthony Pagano <apagano51@gmail.com>
2019-08-11 16:07:26 -0400

    master: adjusted seed data, added db and client vars

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost:27017/dlr-quotes';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/dlr-quotes';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.DATABASE_URL = 'mongodb://apagano76:76apagano@ds261567.mlab.com:21282/dlr-quotes';
 exports.PORT = process.env.PORT || 8080;
