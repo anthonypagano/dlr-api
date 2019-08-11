824906f4a3b113379ccd9225f5d36c01d7d281a6
Anthony Pagano <apagano51@gmail.com>
2019-08-11 16:10:26 -0400

    master: adjusted db var

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
-exports.DATABASE_URL = 'mongodb://apagano76:76apagano@ds261567.mlab.com:21282/dlr-quotes';
+exports.DATABASE_URL = 'mongodb://apagano76:76apagano@ds261567.mlab.com:61567/dlr-quotes';
 exports.PORT = process.env.PORT || 8080;
